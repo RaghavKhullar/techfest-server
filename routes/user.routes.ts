7c5c8870776602030652613c40e9550d1de1dabb
Raghav Khullar <khullarraghav2001@gmail.com>
2023-12-24 09:36:43 +0530

    feat(api): User controller additions + Minor fixes

--- routes/user.routes.ts
+++ routes/user.routes.ts
@@ -2,7 +2,15 @@ import express from "express";
 const userRouter = express.Router();
 
 import { userLogout, userLogin } from "../controller/auth.controller";
+import {
+    getCurrentUser,
+    getDetailsForAnalytics,
+} from "../controller/user.controller";
+import { authenticateUserToken } from "../utils/verifyToken";
 
 userRouter.get("/callback", userLogin);
 userRouter.get("/logout", userLogout);
+
+userRouter.get("/getDetails", authenticateUserToken, getCurrentUser);
+userRouter.get("/getUserStats", authenticateUserToken, getDetailsForAnalytics);
 export default userRouter;
